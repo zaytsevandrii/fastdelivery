d3c926147e47b08461f2621f3b35d00a5ec9763b
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-10-30 13:08:16 +0000

    Search_Lodash_debounce2

---
+++ src/UsedInProject/UsedInTheProject.jsx
@@ -0,0 +1,12 @@
+Used in the project
+React v18
+UseState, UseEffect, UseContext, UseCallback, UseRef, UseSelector,UseDispatch
+Redux Toolkit
+SkeletonReact
+ReactRouter
+React Paginate
+Lodash debounce 
+Axios
+Mockapi.io
+Sass
+Icons from https://www.iconfinder.com/
